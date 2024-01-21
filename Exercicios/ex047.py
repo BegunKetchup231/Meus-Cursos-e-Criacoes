a7efc48538ec89931e88aa0d2fe4c6eb5f873b7e
BegunKetchup231 <142554725+BegunKetchup231@users.noreply.github.com>
2024-01-21 09:28:06 -0300

    Resolvendo exercício 47

--- Exercicios/ex047.py
+++ Exercicios/ex047.py
@@ -1,4 +1,8 @@
 # Crie um program que mostre na tela todos os números pares
 # que estão no intervalo entre 1 e 50.
 
+print("Esses são todos os números pares entre 1 e 50")
 
+for numeros in range(1, 51):
+    if numeros % 2 == 0:
+        print(numeros)
