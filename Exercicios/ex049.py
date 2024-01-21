03fb322db9308f079ecae6c602b8b2cb99584221
BegunKetchup231 <142554725+BegunKetchup231@users.noreply.github.com>
2024-01-21 10:10:55 -0300

    Resolvendo exercício 49

--- Exercicios/ex049.py
+++ Exercicios/ex049.py
@@ -1,23 +1,13 @@
 # Refaça o desafio 009, mostrando a tabuada de um número que o usuário
 # escolher. Só que agora utilizando um laço for.
 
-# Pede ao usuário para digitar um número
-num = int(input("Digite um número para saber sua tabuada: "))
+print("Tabuada dos Números!!")
 
-# Define a multiplicação ao valor de 0
-multi = 0
+numero = int(input("Digite um número: "))
 
-# Print estético da calculadora
-print(("-" * 10), "Tábuada do", f"{num}", ("-" * 10))
+multiplicacao = 0
+print(f"A tabuada do número {numero} é: ")
 
-# Define por quanto o "num" será multiplicado, no caso por 1 até 10
-for quant in range (0, 11) :
-
-    # Faz a multiplicação da quantidade pelo número 
-    multi = (quant * num) 
-
-    # Mostra na tela o resultado do num x 1, 2 etc até 10...
-    print("-+- "f"{num} x {quant:2} = {multi}")
-
-# Print estético
-print("-" * 36)
+for quantidade in range(0, 11):
+    multiplicacao = quantidade * numero
+    print(f"{numero} x {quantidade:2} = {multiplicacao}")
