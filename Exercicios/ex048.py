6d53683fc5badd6847b34363ad2b0bde0f8f4ce1
BegunKetchup231 <142554725+BegunKetchup231@users.noreply.github.com>
2024-01-22 08:34:56 -0300

    Resolvendo exercício 48

--- Exercicios/ex048.py
+++ Exercicios/ex048.py
@@ -1,12 +1,11 @@
-# Faça um program que calcule a soma entre todos os números ímpares
-# que são múltiplos de três e que se encontram no intervalo entre 1 até 500.
+# Faça um program que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo entre 1 até 500.
 
 soma = 0
+contagem = 0
 
 for numeros in range(1, 501):
-    if numeros % 2 != 0 and numeros % 3 ==0:
+    if numeros % 2 != 0 and numeros % 3 == 0:
         soma += numeros
-
-print("A soma de todos os números é de:")
-print(soma)
- 
+        contagem += 1
+        
+print(f"\033[93mA soma dos {contagem} valores multiplos de 3 e ímpares é de: {soma}\033[0m")
