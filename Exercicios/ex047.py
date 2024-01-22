584fda1fa5ae788b203decacb118e08ec0488a93
BegunKetchup231 <142554725+BegunKetchup231@users.noreply.github.com>
2024-01-22 08:24:32 -0300

    Adicionando opções novas nos mesmos exercícios

--- Exercicios/ex047.py
+++ Exercicios/ex047.py
@@ -1,8 +1,16 @@
 # Crie um program que mostre na tela todos os números pares
 # que estão no intervalo entre 1 e 50.
 
+# Maneira 1
 print("Esses são todos os números pares entre 1 e 50")
 
 for numeros in range(1, 51):
     if numeros % 2 == 0:
-        print(numeros)
+        print(numeros, end=" ")
+
+
+# Maneira 2 (Menos processamento)
+print("Esses são todos os números pares entre 1 e 50")
+
+for numeros in range(2, 51, 2):
+    print(numeros, end=" ")
