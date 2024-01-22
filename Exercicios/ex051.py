45f01259df38e2cba1b398ce72d3def092ef7bec
BegunKetchup231 <142554725+BegunKetchup231@users.noreply.github.com>
2024-01-22 09:12:02 -0300

    Resolvendo exercício 51

--- Exercicios/ex051.py
+++ Exercicios/ex051.py
@@ -1,3 +1,13 @@
-# Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
-# No final, mostre os 10 primeiros termos dessa progressão
+# Desenvolva um programa que leia o primeiro termo e a razão de uma PA e no final, mostre os 10 primeiros termos dessa progressão
 
+print("10 Primeiros termos da PA")
+
+primeiro = int(input("Digite o primeiro termo: "))
+razao = int(input("Digite a Razão da PA: "))
+
+#10 termo
+decimo = primeiro + (10 - 1) * razao
+
+print(f"\033[95mOs 10 primeiros termos da PA são:\033[0m")
+for termos in range(primeiro, decimo + 1, razao):
+    print(f"\033[93m{termos}\033[0m", end=" ")
