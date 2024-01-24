c4f1a0353aad4a7fd58f479eccfda114925030d3
BegunKetchup231 <142554725+BegunKetchup231@users.noreply.github.com>
2024-01-24 07:34:54 -0300

    Resolvendo exercício 56

--- Exercicios/ex056.py
+++ Exercicios/ex056.py
@@ -3,4 +3,32 @@
 # Qual é o nome do homem mais velho;
 # Quantas mulheres têm menos de 20 anos.
 
-TESTE
+somaidade = 0
+mediaidade = 0
+maioridadeh = 0
+nomemaisvelho = ""
+totalmulher20 = 0
+
+for pessoas in range(1 , 5):
+    print(f"_____ {pessoas}a Pessoa _____")
+    nome = str(input("Digite o nome: ")).upper().strip()
+    idade = int(input("Digite a idade: "))
+    sexo = str(input("Digite o sexo, [M/F]: ")).upper().strip()
+    somaidade += idade
+
+#   > Segunda forma
+#   if pessoas == 1 and sexo in "Mm":
+    if pessoas == 1 and sexo == "M":
+        maioridadeh = idade
+        nomemaisvelho = nome
+    if sexo in "M" and idade > maioridadeh:
+        maioridadeh = idade
+        nomemaisvelho = nome
+    if sexo in "F" and idade < 20:
+        totalmulher20 += 1
+
+mediaidade = somaidade / 4
+
+print(f"A média de idade do grupo é de {mediaidade} anos")
+print(f"O homem mais velho tem {maioridadeh} e se chama {nomemaisvelho}")
+print(f"Ao todo são {totalmulher20} mulheres com menos de 20 anos de idade")
